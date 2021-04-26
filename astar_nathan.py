e46cd481b16a240a2a2f77218b49d2f9b15dea4a
teadetime <ntfaber@gmail.com>
2021-04-26 17:29:23 -0400

    graph examples and runtime list

--- astar_nathan.py
+++ astar_nathan.py
@@ -2,6 +2,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import math
 from queue import PriorityQueue
+import copy as cp
 
 def build_graph(vis=False):
     G = nx.Graph()
@@ -25,6 +26,12 @@ def build_graph(vis=False):
     return G
 
 def astar(G, start_node, end_node):
+    """
+    Output:
+    list containing the nddes to take to the destination: [1, 2, 3]
+    output runlist: [(set(), [1]), ({1}, [2, 4]), ({1, 2}, [4, 3]), ({1, 2, 4}, [3])]
+    list containing the visited nbodes at each step as well as the ones that are candidates for exploring! stored as a list of tuples that conatin a set and a list
+    """
     #*******#
     #*Setup*#
     #*******#
@@ -40,7 +47,7 @@ def astar(G, start_node, end_node):
             best_dist = math.inf
         node_dict[node] = best_dist
             
-
+    run_list = []
     # Initilize the starting node into visited
     p_queue = PriorityQueue()
     visited = set()
@@ -48,11 +55,12 @@ def astar(G, start_node, end_node):
     p_queue.put((heuristic(G,start_node, end_node), start_node))
 
     while not p_queue.empty():
+        ## Addd stuff into plotting dict
+        run_list.append(((cp.copy(visited)), cp.copy([b for (a,b) in p_queue.queue])))
+
         short = p_queue.get()
         node = short[1]
         node_pri = short[0]
-        print(node)
-        print(node_pri)
         if node == end_node: break
         visited.add(node)
         dist_to_node = node_dict[node] ## NOT SURE THIS IS RIGHT
@@ -67,7 +75,7 @@ def astar(G, start_node, end_node):
                 node_dict[other_node] = new_dist
                 prev_map[other_node] = node
                 p_queue.put((new_dist + heuristic(G,start_node, end_node), other_node))
-
+        
     if end_node not in prev_map:
         return []
 
@@ -79,7 +87,7 @@ def astar(G, start_node, end_node):
         min_path.append(prev_vertex)
         curr_vertex = prev_vertex
     min_path.reverse()
-    return min_path
+    return (min_path, run_list)
   
 def heuristic(g, cur_node, end_node):
     cur_pos = g.nodes()[cur_node]["pos"]
@@ -88,6 +96,51 @@ def heuristic(g, cur_node, end_node):
     dy = abs(cur_pos[1]-end_pos[1])
     return (dx+dy)
 
+
+def plot_video(G, run_data):
+    ## Do setup of stuff here
+    frame_num = 0
+    ## Loop through each state of ASTAR
+    for frame in run_data:
+        # Process this into a mtaplotlib call
+        build_matplotlib(G, run_data, frame_num)
+        frame_num += 1
+
+    # Call ffmpeg to make a video
+    # import os
+    # import imageio
+
+    # png_dir = '../animation/png'
+    # images = []
+    # for file_name in sorted(os.listdir(png_dir)):
+    #     if file_name.endswith('.png'):
+    #         file_path = os.path.join(png_dir, file_name)
+    #         images.append(imageio.imread(file_path))
+    # imageio.mimsave('../animation/gif/movie.gif', images)
+
+def build_matplotlib(G, astar_data, frame_num):
+    visited = astar_data[0] # This is a set of visited nodes
+    in_queue = astar_data[1] # List of nodes in pqueue in order!
+
+    for node in G.nodes():
+        # Do some plotting
+        pass
+    for vist_node in visited:
+        vist_node = G[vist_node]
+        # Plot operation
+    for explore_node in in_queue:
+        explore_node = G[explore_node]
+        #plot operation
+    
+    plt.savefig("images/file%02d.png" % frame_num)
+
+
+def route_find():
+    pass
+
+
 if __name__ == "__main__":
     graph = build_graph(False)
-    print(astar(graph, 1, 3))
+    ret = astar(graph, 1, 3)
+    print(ret[0])
+    print(ret[1])
