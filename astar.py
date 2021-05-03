e51e3627e04e8640e69400149ab242e43cf16a7a
Mickmcginnis <zoekauai@gmail.com>
2021-05-03 17:29:39 -0400

    refactor build_graph for visualization, create visualize_graph to display static best path graph

--- astar.py
+++ astar.py
@@ -137,46 +137,44 @@ def add_dist_edge(graph, node1, node2, unit = "ft"):
 
     graph.add_edge(node1, node2, weight = round(haversine(coords1,coords2, unit=unit),1))
     
-def build_graph(vis=False, csv_loc='node_connections.csv', csv_path_nodes ='path_nodes.csv',  directed=False):
+def build_graph(vis=False, csv_loc='node_connections.csv', csv_path_nodes ='path_nodes.csv', directed=False):
     if directed:
         G = nx.DiGraph()
     else:
         G = nx.Graph()
-    G.add_node("AC 1",    pos=(324, 403),  coords = (42.29321996991126, -71.26459288853798))  #  AC 1
-    G.add_node("AC 2",    pos=(456, 240),  coords = (42.29363418081095, -71.26422001841294))  # AC 2
-    G.add_node("AC 3",    pos=(599, 149),  coords = (42.29386690104804, -71.2637380411814))   # AC 3
-    G.add_node("AC 4",    pos=(404, 160),  coords = (42.29382686013987, -71.26440227411027))  # AC 4
-    G.add_node("CC 1",    pos=(653, 376),  coords = (42.29328713953713, -71.26356098757215))  # CC 1
-    G.add_node("CC 2",    pos=(717, 396),  coords = (42.29322457502598, -71.26333431385294))  # CC 2 (lower)
-    G.add_node("CC 3",    pos=(758, 266),  coords = (42.29354657304486, -71.26318545348332))  # CC 3 (lower)
-    G.add_node("CC 4",    pos=(636, 154),  coords = (42.29384938315572, -71.2636004581119))  # CC 4
-    G.add_node("MH 1",    pos=(499, 488),  coords = (42.2930076842383, -71.26406733829359))  # MH 1
-    G.add_node("MH 2",    pos=(582, 536),  coords = (42.29290340955621, -71.26378991671152))  # MH 2
-    G.add_node("MH 3",    pos=(549, 645),  coords = (42.29261011959146, -71.26390792634388))  # MH 3
-    G.add_node("WH 1",    pos=(830, 482),  coords = (42.293019830361196, -71.26295440109273))  # WH 1 (in)
-    G.add_node("WH 2",    pos=(768, 427),  coords = (42.29318649160559, -71.26315556675328))  # WH 2 w
-    G.add_node("WH 3",    pos=(911, 539),  coords = (42.29290277041453, -71.2627009323597))  # WH 3 e
-    G.add_node("WH 4",    pos=(846, 432),  coords = (42.29314978649178, -71.26289941581034))  # WH 4 w lounge
-    G.add_node("WH 5",    pos=(876, 474),  coords = (42.29305157539783, -71.26279078634431))  # WH 5 e lounge
-    G.add_node("WH 6",    pos=(909, 323),  coords = (42.29341267402442, -71.26270361455002))  # WH 6 n
-    G.add_node("EH 1",    pos=(1030, 649), coords = (42.29261111163257, -71.26224763901361))  # EH 1 (in)
-    G.add_node("EH 2",    pos=(976, 576),  coords = (42.29278875392011, -71.26247385140057))  # EH 2 w
-    G.add_node("EH 3",    pos=(1138, 703), coords = (42.29239031457536, -71.26195038758173))  # EH 3 e
-    G.add_node("EH 4",    pos=(1063, 594), coords = (42.292724584494934, -71.26214138449437))  # EH 4 (in) w lounge
-    G.add_node("EH 5",    pos=(1093, 619), coords = (42.292646745688444, -71.26204581431963))  # EH 5 e lounge
-    G.add_node("EH 6",    pos=(1086, 482), coords = (42.293027263579965, -71.26207940607478))  # EH 6 n   # CHECK
-    G.add_node("LPB",     pos=(248, 179),  coords = (42.293764694557765, -71.26489403407126))  # LPB
-    G.add_node("Park 1",  pos=(196, 508),  coords = (42.29295691903568, -71.26511968235003))  # parking lot A
-    G.add_node("TC",      pos=(366, 602),  coords = (42.29263151086271, -71.26455375403833))  # traffic circle
-    G.add_node("Park 2",  pos=(1198, 612), coords = (42.29266880578926, -71.26171318420484))  # parking lot B
+    G.add_node("AC 1",    pos=(324, 403),  coords = (42.29321996991126, -71.26459288853798), pathway=False)  #  AC 1
+    G.add_node("AC 2",    pos=(456, 240),  coords = (42.29363418081095, -71.26422001841294), pathway=False)  # AC 2
+    G.add_node("AC 3",    pos=(599, 149),  coords = (42.29386690104804, -71.2637380411814), pathway=False)   # AC 3
+    G.add_node("AC 4",    pos=(404, 160),  coords = (42.29382686013987, -71.26440227411027), pathway=False)  # AC 4
+    G.add_node("CC 1",    pos=(653, 376),  coords = (42.29328713953713, -71.26356098757215), pathway=False)  # CC 1
+    G.add_node("CC 2",    pos=(717, 396),  coords = (42.29322457502598, -71.26333431385294), pathway=False)  # CC 2 (lower)
+    G.add_node("CC 3",    pos=(758, 266),  coords = (42.29354657304486, -71.26318545348332), pathway=False)  # CC 3 (lower)
+    G.add_node("CC 4",    pos=(636, 154),  coords = (42.29384938315572, -71.2636004581119), pathway=False)  # CC 4
+    G.add_node("MH 1",    pos=(499, 488),  coords = (42.2930076842383, -71.26406733829359), pathway=False)  # MH 1
+    G.add_node("MH 2",    pos=(582, 536),  coords = (42.29290340955621, -71.26378991671152), pathway=False)  # MH 2
+    G.add_node("MH 3",    pos=(549, 645),  coords = (42.29261011959146, -71.26390792634388), pathway=False)  # MH 3
+    G.add_node("WH 1",    pos=(830, 482),  coords = (42.293019830361196, -71.26295440109273), pathway=False)  # WH 1 (in)
+    G.add_node("WH 2",    pos=(768, 427),  coords = (42.29318649160559, -71.26315556675328), pathway=False)  # WH 2 w
+    G.add_node("WH 3",    pos=(911, 539),  coords = (42.29290277041453, -71.2627009323597), pathway=False)  # WH 3 e
+    G.add_node("WH 4",    pos=(846, 432),  coords = (42.29314978649178, -71.26289941581034), pathway=False)  # WH 4 w lounge
+    G.add_node("WH 5",    pos=(876, 474),  coords = (42.29305157539783, -71.26279078634431), pathway=False)  # WH 5 e lounge
+    G.add_node("WH 6",    pos=(909, 323),  coords = (42.29341267402442, -71.26270361455002), pathway=False)  # WH 6 n
+    G.add_node("EH 1",    pos=(1030, 649), coords = (42.29261111163257, -71.26224763901361), pathway=False)  # EH 1 (in)
+    G.add_node("EH 2",    pos=(976, 576),  coords = (42.29278875392011, -71.26247385140057), pathway=False)  # EH 2 w
+    G.add_node("EH 3",    pos=(1138, 703), coords = (42.29239031457536, -71.26195038758173), pathway=False)  # EH 3 e
+    G.add_node("EH 4",    pos=(1063, 594), coords = (42.292724584494934, -71.26214138449437), pathway=False)  # EH 4 (in) w lounge
+    G.add_node("EH 5",    pos=(1093, 619), coords = (42.292646745688444, -71.26204581431963), pathway=False)  # EH 5 e lounge
+    G.add_node("EH 6",    pos=(1086, 482), coords = (42.293027263579965, -71.26207940607478), pathway=False)  # EH 6 n   # CHECK
+    G.add_node("LPB",     pos=(248, 179),  coords = (42.293764694557765, -71.26489403407126), pathway=False)  # LPB
+    G.add_node("Park 1",  pos=(196, 508),  coords = (42.29295691903568, -71.26511968235003), pathway=False)  # parking lot A
+    G.add_node("TC",      pos=(366, 602),  coords = (42.29263151086271, -71.26455375403833), pathway=False)  # traffic circle
+    G.add_node("Park 2",  pos=(1198, 612), coords = (42.29266880578926, -71.26171318420484), pathway=False)  # parking lot B
 
     with open(csv_path_nodes, newline='') as csvfile:
         csv_reader = csv.reader(csvfile, delimiter=',')
         for row in csv_reader:
             if len(row) > 3:
-                G.add_node(row[0], coords=(float(row[1]), float(row[2])), pos=(int(row[3]), int(row[4])))
-            elif len(row) > 2:
-                G.add_node(row[0], coords=(float(row[1]), float(row[2])))
+                G.add_node(row[0], coords=(float(row[1]), float(row[2])), pos=(int(row[3]), int(row[4])), pathway="True")
 
     with open(csv_loc, mode='r') as csv_file:
         edge_counter = set()
@@ -205,22 +203,43 @@ def build_graph(vis=False, csv_loc='node_connections.csv', csv_path_nodes ='path
                         else:
                             print(f"{cur_node} is not in the networkx graph!")
 
-
         print(f'Added {len(edge_counter)} Unique edges to the graph from {csv_loc}')
+        return G
 
+def visualize_graph(G, node_list, start_node, end_node):
+    """
+    Output: Networkx graph of shortest path
+    Displays the shortest path graph using matplotlib
+    """
+    all_nodes = list(G.nodes())
+    node_sizes = []
+    node_labels = {}
+    pos = nx.get_node_attributes(G, 'pos')
+    pathway = nx.get_node_attributes(G, 'pathway')
+    for i in all_nodes:
+        if i != start_node and i != end_node:
+            if i in node_list:
+                node_pos = pos[i]
+                if pathway[i]:
+                    node_sizes.append(0)
+                    node_labels[str(i)] = ""
+                else:
+                    node_sizes.append(300)
+                    node_labels[str(i)] = str(i)
+            else:
+                G.remove_node(i)
+        else:
+            node_sizes.append(300)
+            node_labels[str(i)] = str(i)
 
-    if vis:
-        node_sizes = []
-        for i in range(27):
-            node_sizes.append(100)
-        plt.figure(figsize=(7, 7))
-        pos= nx.get_node_attributes(G, 'pos')
-        nx.draw_networkx(G, pos, node_size = node_sizes)
-        labels = nx.get_edge_attributes(G,'weight')
-        nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
-        data = mpimg.imread('./images/olin_sat.png')
-        plt.imshow(data)
-        plt.show()
+    plt.figure(figsize=(10, 8))
+    pos = nx.get_node_attributes(G, 'pos')
+    nx.draw_networkx(G, pos, labels=node_labels, node_size=node_sizes, node_color='red', font_size=12)#, font_color='white')
+    # labels = nx.get_edge_attributes(G, 'weight')
+    nx.draw_networkx_edges(G, pos, width=3, edge_color="blue")
+    data = mpimg.imread('./images/olin_sat.png')
+    plt.imshow(data)
+    plt.show()
     return G
 
 def ordered_route(G, ordered_route):
@@ -243,8 +262,12 @@ if __name__ == "__main__":
 
     g = build_graph(vis=False, directed=False)
 
+    Start = "AC 1"
+    End = "WH 1"
+    A = astar(g, Start, End)
+    visualize_graph(g, A[0], Start, End)
     # ret = astar(g, "Park 1", "AC 4")
     # print(ret[0])
     # print(ret[1])
-    ret = ordered_route(g, ["AC 1", "LPB", "AC 4"])
-    print(ret)
+    # ret = ordered_route(g, ["AC 1", "LPB", "AC 4"])
+    # print(ret)
