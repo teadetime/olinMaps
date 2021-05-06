b3dd8b296ce26cdc131ae2f90dfcd7dab732870c
Mickmcginnis <zoekauai@gmail.com>
2021-05-06 10:01:11 -0400

    add comments

--- animate.py
+++ animate.py
@@ -10,21 +10,29 @@ def call_vis(k, start_node, end_node, c, draw_color):
 
 def get_figs(start_node, end_node):
     images = []
+    # where to save image
     img="./images/ani/img.png"
     g = build_graph()
     A = astar(g, start_node, end_node)
+    # use the "process" part of the astar output
     explored = A[1]
 
     for i in explored:
+        # call the stupid function I made at some point just calling 
+        # visualize_graph, I forget why will correct later
         call_vis(i[0], start_node, end_node, img, 'orange')
+        # append the image to list of images to use in animation
         images.append(imageio.imread(img))
+        # clean the image dir
         remove(img)
 
+    # display final solution for twice as long as normal frame
     for i in range(2):
         call_vis(A[0], start_node, end_node, img, 'red')
         images.append(imageio.imread(img))
         remove(img)
 
+    # compile the animation from images list
     imageio.mimsave('./images/animation.gif', images, duration=.6)
 
 
